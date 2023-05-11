89e282d9c36a8d55573702a27108c6cc877b2fd0
André Sizer <andresizer@gmail.com>
2023-05-11 14:12:12 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int numRandomico() {
+    // Configura a semente do gerador de números aleatórios
+    srand(time(NULL));
+
+    // Gera um número aleatório entre 1 e 100
+   int numeroAleatorio = (rand() % 100) + 1;
+    
+    // A função retorna o número aleatório gerado
+    return numeroAleatorio;
+}
+
+int main () {
+restart:
+int numero;
+int i = 10;
+char r;
+int numCerto = numRandomico();
+    // Entra em um laço de repetição e permanece nele se o número estiver errado ou as tentativas não forem esgotadas
+    while(numCerto != numero && i > 0){
+        // i é o numero de tentativas. começa com 10 e após a 1ª tentativa vai para 9
+        if (i == 10){
+            printf("Seja bem vindo ao jogo de adivinhação!\nVocê consegue adivinhar qual número eu estou pensando?\nVocê tem 10 tentativas!\n");
+            printf("Dica: É um número de 1 a 100\n");
+            scanf("%i",&numero);
+            i = 9;
+        // após a 1ª tentativa entra na condicional,     
+        }else if (i < 10){
+            // se o número digitado for menor que 1 ou maior que 100, avisa que o número é inválido e não desconta uma jogada
+            if (numero < 1 || numero >100){
+                printf("Digite um número entre 1 e 100!\n");
+                printf("Tente novamente!\n");
+                i++;
+            }
+            // se o número for maior que o número certo, o jogo avisa que o número certo é menor e desconta uma jogada
+            if (numero > numCerto){
+                printf("Errou! Tente um número é menor!\n");
+                printf("Você tem %i tentativas!\nTente novamente: ",i);
+                scanf("%i",&numero);
+                i--;
+            // se o número for menor que o número certo, o jogo avisa que o número certo é maior e desconta uma jogada    
+            }else if(numero < numCerto){
+                printf("Errou! Tente um número é maior!\n");
+                printf("Você tem %i tentativas!\nTente novamente: ",i);
+                scanf("%i",&numero);
+                i--;
+            }
+        }
+    }
+    // ao sair do loop, o jogo verifica se saiu porque acertou o número ou porque esgotou as jogadas.
+    //
+    //Vitória! O jogo identifica a vitória e avisa qual o número e quantas jogadas foram necessárias para terminar.
+    if (numCerto == numero){
+        printf("Parabéns! Você acertou!\n");
+        printf("Eu estava pensando no número %d\n", numCerto);
+        printf("Você demorou %i tentativas para acertar!",10-i);
+        return 0;
+    //Derrota :( nesse caso o jogo pergunta se o jogador quer recomeçar a partida.    
+    }else if(i == 0){
+            printf("Você perdeu, tentativas esgotadas! Deseja começar novamente? Digite s ou n\n");
+            scanf(" %c",&r);
+            //enquanto não for digitado s ou n o loop permanece
+            while(r != 's' && r != 'n'){ 
+                printf("Caractér inválido, digite novamente s ou n: \n");
+                scanf(" %c",&r);
+            }
+            // caso seja digitado 's', o jogo recomeça, indo para o restart, que fica logo abaixo do main().
+            if(r == 's'){
+                goto restart;
+            // caso seja digitado 'n', o jogo termina e se despede do jogador.    
+            }else if(r == 'n'){
+                printf("Até logo!");
+                return 0;
+            }
+        }
+}
